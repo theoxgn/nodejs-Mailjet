f4ec270cbdd56a4efb5fd2946c8937d65709cdc2
theoxgn <aaluff112@gmail.com>
2024-07-03 15:39:09 +0700

    add variabel and fix the function

---
+++ src/README.md
@@ -0,0 +1,31 @@
+
+# Mailjet Example with Node.js
+
+## Description
+
+This repository provides an example of using Mailjet with Node.js.
+
+## Requirements
+
+- Node.js
+- Key Mailjet
+- Yarn or npm for package management
+
+## Setting up the API Key
+Before you can use the Mailjet API, you must first obtain an API key. If you don't already have one, create a key with one click in Mailjet website.
+[Get API](https://app.mailjet.com/account/apikeys)
+
+## Run the application locally 
+
+1. Make sure you have generated the API key as shown above. Please make sure to use and store this key securely. 
+
+2. Install the package using 
+```npm install```
+
+3. Create an .env file then fill it with variables API_KEY and SECRET_KEY
+
+4. Run this using 
+```npm start```
+
+## Documentation 
+1. You can see detailed API Reference for the Mailjet APIs [here](https://dev.mailjet.com/email/guides/) 
--- src/index.js
+++ src/index.js
@@ -1,28 +1,34 @@
 var express = require('express');
 require('dotenv').config();
 var router = express.Router();
-const mailjet = require('node-mailjet').connect(process.env.API_KEY, process.env.SECRET_KEY)
+
+const mailjetClient = require('node-mailjet');
+const apiKey = process.env.API_KEY;
+const secretKey = process.env.SECRET_KEY;
+
+const mailjet = mailjetClient.apiConnect(apiKey, secretKey);
 
 router.post('/send-mail', function (req, res) {
-    const contentEmail = `
-        <h1>Hai ${req.body.name},</h1>
-        <p>Selamat, kamu telah berhasil mengirim email menggunakan Mailjet!</p>
-    `
+    // const contentEmail = `
+    //     <h1>Hai ${req.body.name},</h1>
+    //     <p>Selamat, kamu telah berhasil mengirim email menggunakan Mailjet!</p>
+    // `
     const request = mailjet
         .post("send", { 'version': 'v3.1' })
         .request({
             "Messages": [{
                 "From": {
-                    "Email": "aaluff112@gmail.com",
+                    "Email": "rindayemima.work@gmail.com",
                     "Name": "test Mailjet"
                 },
                 "To": [{
                     "Email": req.body.email, 
                     "Name": req.body.name    
                 }],
-                "Subject": "Selamat, email kamu berhasil terkirim",
-                "HTMLPart": contentEmail
-            }]
+                "Subject": "Your email flight plan!",
+						"TextPart": "Dear passenger 1, welcome to Mailjet! May the delivery force be with you!",
+						"HTMLPart": "<h3>Dear passenger 1, welcome to <a href=\"https://www.mailjet.com/\">Mailjet</a>!</h3><br />May the delivery force be with you!"
+				}]
         })
     request
         .then((result) => {
